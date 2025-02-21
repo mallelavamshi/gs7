4ce92e93b65bcb37ecdb6fcaed92599cbb49a95f
“mallelavamshigit config --global user.email “m.vamshikrishna22@gmail.com”git config --global user.email “m.vamshikrishna22@gmail.com”fyfyq-qclsgit exitgit cls2442344545 244234 4545git config --global user.name “mallelavamshi <m.vamshikrishna22@gmail.com>
2025-02-21 00:24:44 -0600

    white2

--- auth_original.py
+++ auth_original.py
@@ -14,7 +14,7 @@ def login_page():
         <style>
             /* Reset all default margins and paddings */
             .stApp {
-                background-color: #000000 !important;
+                background-color: #FFFFFF !important;
                 padding: 0 !important;
                 margin: 0 !important;
                 margin-top: -2rem !important;
